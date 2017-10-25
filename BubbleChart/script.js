40b3614e1c1dd8e5ce1b2f0de1c592f275c1354d
designbright <ukmills12@gmail.com>
2017-10-25 14:38:07 -0400

    adding bubble chart

--- BubbleChart/script.js
+++ BubbleChart/script.js
@@ -8,23 +8,67 @@ var latinAmerica = [40,20,10,16,24,38,74,167,508,784];
 var northAmerica = [6,3,2,2,7,26,82,172,312,433];
 
 
-new Chart(document.getElementById("horizontalBarChart"), {
-    type: 'horizontalBar',
+new Chart(document.getElementById("bubbleChart"), {
+    type: 'bubble',
     data: {
-      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+      labels: "Africa",
       datasets: [
         {
-          label: "Population (millions)",
-          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-          data: [2478,5267,734,784,433]
+          label: ["China"],
+          backgroundColor: "rgba(255,221,50,0.2)",
+          borderColor: "rgba(255,221,50,1)",
+          data: [{
+            x: 21269017,
+            y: 5.245,
+            r: 15
+          }]
+        }, {
+          label: ["Denmark"],
+          backgroundColor: "rgba(60,186,159,0.2)",
+          borderColor: "rgba(60,186,159,1)",
+          data: [{
+            x: 258702,
+            y: 7.526,
+            r: 10
+          }]
+        }, {
+          label: ["Germany"],
+          backgroundColor: "rgba(0,0,0,0.2)",
+          borderColor: "#000",
+          data: [{
+            x: 3979083,
+            y: 6.994,
+            r: 15
+          }]
+        }, {
+          label: ["Japan"],
+          backgroundColor: "rgba(193,46,12,0.2)",
+          borderColor: "rgba(193,46,12,1)",
+          data: [{
+            x: 4931877,
+            y: 5.921,
+            r: 15
+          }]
         }
       ]
     },
     options: {
-      legend: { display: false },
       title: {
         display: true,
         text: 'Predicted world population (millions) in 2050'
+      }, scales: {
+        yAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: "Happiness"
+          }
+        }],
+        xAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: "GDP (PPP)"
+          }
+        }]
       }
     }
 });
